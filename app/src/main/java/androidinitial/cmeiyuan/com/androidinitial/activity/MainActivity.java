909d7bf56cabdd45ace455de64678c65a59826e7
cmeiyuan <cmeiyuan@126.com>
2018-08-05 18:10:52 +0800

    添加common模块

---
+++ app/src/main/java/androidinitial/cmeiyuan/com/androidinitial/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package androidinitial.cmeiyuan.com.androidinitial.activity;
+
+import android.os.Bundle;
+
+import com.meiyuan.module.common.activity.BaseActivity;
+
+import androidinitial.cmeiyuan.com.androidinitial.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ module/common/src/main/java/com/meiyuan/module/common/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.meiyuan.module.common.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cmeiyuan on 2018/8/5
+ */
+public class BaseActivity extends AppCompatActivity {
+}
